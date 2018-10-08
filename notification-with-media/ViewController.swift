6d05db14c47addde896be8b1c75b77cf733e360b
Gabriel Palhares <palhariel@10-45-48-208.0200.ifce.br.iosda.org>
2018-10-08 09:47:10 -0300

    Initial Commit

---
+++ notification-with-media/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  notification-with-media
+//
+//  Created by Gabriel Palhares on 08/10/18.
+//  Copyright © 2018 Gabriel Palhares. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ notification-with-mediaTests/notification_with_mediaTests.swift
@@ -0,0 +1,34 @@
+//
+//  notification_with_mediaTests.swift
+//  notification-with-mediaTests
+//
+//  Created by Gabriel Palhares on 08/10/18.
+//  Copyright © 2018 Gabriel Palhares. All rights reserved.
+//
+
+import XCTest
+@testable import notification_with_media
+
+class notification_with_mediaTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
