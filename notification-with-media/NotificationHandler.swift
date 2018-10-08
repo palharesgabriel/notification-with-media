f1047deeba8e814e57843b23f032dc6f2eab65e2
Gabriel Palhares <gaapalhares@gmail.com>
2018-10-08 17:32:53 -0300

    Notificação teste com uma imagem anexada

--- notification-with-media/NotificationHandler.swift
+++ notification-with-media/NotificationHandler.swift
@@ -14,9 +14,53 @@ class NotificationHandler {
     static let shared = NotificationHandler()
     private init() {}
     
+    
     func requestAuthorization() {
         
-        UNUserNotificationCenter.current().requestAuthorization(options: <#T##UNAuthorizationOptions#>, completionHandler: <#T##(Bool, Error?) -> Void#>)
+        UNUserNotificationCenter.current().requestAuthorization(options: [.badge, .alert, .sound]) { (succeded, error) in
+            
+            if(succeded) {
+                print("Permission granted")
+            }
+            else {
+                print("Permission denied")
+            }
+        }
+        
+    }
+    
+    
+    func createNotification() -> UNNotificationRequest {
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Bom dia seu lindo"
+        content.subtitle = "Tá na hora do show em"
+        content.body = "blablaalblablablalbalbalblablalbalblablalbalbl"
+        
+        let urlImage = Bundle.main.url(forResource: "image", withExtension: "jpg")
+        
+        do {
+            content.attachments = try [UNNotificationAttachment(identifier: "image", url: urlImage!, options: nil)]
+        } catch {
+            print(error.localizedDescription)
+        }
+       
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 20, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: "teste", content: content, trigger: trigger)
+        
+        return request
+    }
+    
+    
+    func registerNotification(_ notificationRequest: UNNotificationRequest) {
+        
+        UNUserNotificationCenter.current().add(notificationRequest) { (error) in
+            if(error != nil) {
+                print("Registration failed")
+            }
+        }
         
     }
     
--- notification-with-media/ViewController.swift
+++ notification-with-media/ViewController.swift
@@ -12,7 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let request = NotificationHandler.shared.createNotification()
+        NotificationHandler.shared.registerNotification(request)
+        
     }
 
 
